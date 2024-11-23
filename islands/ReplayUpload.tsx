9be77c1a0b3ad654ab2e9aa1559a051a9b63a2d8
Zach Auten <zachauten@gmail.com>
2024-11-23 15:11:50 -0500

    linting

--- islands/ReplayUpload.tsx
+++ islands/ReplayUpload.tsx
@@ -4,48 +4,45 @@ import { asset } from "$fresh/runtime.ts";
 import { useEffect, useState } from "preact/hooks";
 import init, { Replay } from "../static/pkg/sc2_parser.js";
 
-
 export default function ReplayUpload() {
-    const [replays, setReplays] = useState<Replay[]>([]);
+  const [replays, setReplays] = useState<Replay[]>([]);
 
   useEffect(() => {
-    // async function initWasm() {
-    //     const url = new URL(asset("/pkg/sc2_parser_bg.wasm"), globalThis.location.toString());
-    //     const wasm = await import(url.toString())
-
-    // }
-    // initWasm()
-        async function initWasm() {
-            const url = new URL(asset("pkg/sc2_parser_bg.wasm"), globalThis.location.toString());
-            const wasm = await fetch(url)
-            await init("pkg/sc2_parser_bg.wasm");
-        }
-        initWasm();
+    async function initWasm() {
+      const url = new URL(
+        asset("pkg/sc2_parser_bg.wasm"),
+        globalThis.location.toString(),
+      );
+      await init(url);
+    }
+    initWasm();
   }, []);
 
   const files = useSignal<File[]>([]);
   if (files.value.length < 1) {
     return <UploadZone files={files} />;
-  } else {    
+  } else {
     useEffect(() => {
-        async function parse() {
-            const replays = files.value.map(async file => {
-            const buffer = await file.arrayBuffer();
-            const bytes = new Uint8Array(buffer);
-            return new Replay(bytes, file.name)
-            });
-            const result = await Promise.all(replays)   
-            setReplays(result); 
-        }
-        parse();
+      async function parse() {
+        const replays = files.value.map(async (file) => {
+          const buffer = await file.arrayBuffer();
+          const bytes = new Uint8Array(buffer);
+          return new Replay(bytes, file.name);
+        });
+        const result = await Promise.all(replays);
+        setReplays(result);
+      }
+      parse();
     });
-      return (
-        <>
-          {replays.map((replay) => <>
+    return (
+      <>
+        {replays.map((replay) => (
+          <>
             <div>{replay.file_path}</div>)
             <div>{replay.parsed.metadata}</div>
-          </>)}
-        </>
-      );
-    }
+          </>
+        ))}
+      </>
+    );
+  }
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,6 @@
-import { useSignal } from "@preact/signals";
-import UploadZone from "../components/UploadZone.tsx";
 import ReplayUpload from "../islands/ReplayUpload.tsx";
 
 export default function Home() {
-  const files = useSignal([]);
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
